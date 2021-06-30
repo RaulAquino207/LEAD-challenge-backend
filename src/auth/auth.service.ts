e7e6f734fae21b378d63b6dfdd6de20d0fbd4566
Raul Aquino <aquinoraul207@gmail.com>
2021-06-30 17:52:40 -0300

    changes in login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { ResultDto } from 'src/dto/result.dto';
 
 @Injectable()
 export class AuthService {
@@ -15,8 +16,11 @@ export class AuthService {
 
     async validateUser(email: string, pass: string): Promise<any> {
         const user_return = await this.userService.findByEmail(email);
-
         const { status : _ , user } = user_return;
+
+        if (_ == false){
+          return null
+        }
 
         const isMatch = await bcrypt.compare(pass, user.password);
         if (user && isMatch) {
@@ -27,9 +31,9 @@ export class AuthService {
       }
 
     async login(user: any) {
-      const payload = { email: user.email, sub: user.id, role : user.role };
-      return {
-        access_token: this.jwtService.sign(payload),
-      };
+        const payload = { email: user.email, sub: user.id, role : user.role };
+        return {
+          access_token : this.jwtService.sign(payload)
+        }
     }
 }
